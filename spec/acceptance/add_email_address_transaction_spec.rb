11838b31a6a40d18ceb3c792c17fe36a803caf26
Andrew Shcheglov <godwindock@gmail.com>
2012-06-17 16:24:02 +0300

    Adding address should persist changes to the repository

--- spec/acceptance/add_email_address_transaction_spec.rb
+++ spec/acceptance/add_email_address_transaction_spec.rb
@@ -1,21 +1,29 @@
 require 'kobza_crm'
+require 'mongobzar'
 
 module KobzaCRM
   module Test
     describe AddEmailAddressTransaction do
+      let(:id_generator) { Mongobzar::BSONIdGenerator.new }
+      let(:person_repository) { InMemoryPersonRepository.new(id_generator) }
+
       before do
-        id_generator = Mongobzar::BSONIdGenerator.new
-        @person_repository = InMemoryPersonRepository.new(id_generator)
-        t = AddPersonTransaction.new('Bob', @person_repository)
+        t = AddPersonTransaction.new('Bob', person_repository)
         t.execute
-        @person = t.person
+        @person_id = t.person.id
+
+        t = AddEmailAddressTransaction.new(@person_id, sample_email, person_repository)
+        t.execute
       end
 
-      it 'adds EmailAddress to Person' do
-        t = AddEmailAddressTransaction.new(@person.id, 'test1@example.com', @person_repository)
-        t.execute
+      let(:sample_email) { 'test1@example.com' }
 
-        @person.addresses.first.email_address.should == 'test1@example.com'
+      it 'adds EmailAddress to Person' do
+        addresses = person_repository.find(@person_id).addresses
+        email_address = addresses.first
+
+        email_address.should be_kind_of(EmailAddress)
+        email_address.email_address.should == sample_email
       end
     end
   end
--- spec/acceptance/add_web_page_address_spec.rb
+++ spec/acceptance/add_web_page_address_spec.rb
@@ -1,22 +1,28 @@
 require 'kobza_crm'
+require 'mongobzar'
 
 module KobzaCRM
   module Test
     describe AddWebPageAddressTransaction do
+      let(:id_generator) { Mongobzar::BSONIdGenerator.new }
+      let(:person_repository) { InMemoryPersonRepository.new(id_generator) }
+      let(:sample_url) { 'http://sample.example.com' }
+
       before do
-        id_generator = Mongobzar::BSONIdGenerator.new
-        @person_repository = InMemoryPersonRepository.new(id_generator)
-        t = AddPersonTransaction.new('Bob', @person_repository)
+        t = AddPersonTransaction.new('Bob', person_repository)
         t.execute
-        @person = t.person
+        @person_id = t.person.id
+
+        t = AddWebPageAddressTransaction.new(@person_id, sample_url, person_repository)
+        t.execute
       end
 
       it 'add WebPageAddress to Person' do
-        sample_url = 'http://sample.example.com'
-        t = AddWebPageAddressTransaction.new(@person.id, sample_url, @person_repository)
-        t.execute
+        addresses = person_repository.find(@person_id).addresses
+        web_page_address = addresses.first
 
-        @person.addresses.first.url.should == sample_url
+        web_page_address.should be_kind_of(WebPageAddress)
+        web_page_address.url.should == sample_url
       end
     end
   end
