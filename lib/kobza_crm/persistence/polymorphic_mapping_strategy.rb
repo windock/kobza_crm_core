fa1558b57d3bee2473633bd87ff45e071587a261
Andrew Shcheglov <godwindock@gmail.com>
2012-07-04 20:32:03 +0300

    Refactoring

--- lib/kobza_crm/persistence/polymorphic_mapping_strategy.rb
+++ lib/kobza_crm/persistence/polymorphic_mapping_strategy.rb
@@ -1,49 +1,49 @@
 module KobzaCRM
   module Persistence
     class PolymorphicMappingStrategy
-      def initialize(mappers)
-        @mappers = mappers
+      def initialize(strategies)
+        @strategies = strategies
       end
 
       def build_dto(domain_object)
-        mapper_for_domain_object(domain_object).build_dto(domain_object)
+        strategy_for_domain_object(domain_object).build_dto(domain_object)
       end
 
       def build_dtos(domain_objects)
         domain_objects.map do |domain_object|
-          mapper_for_domain_object(domain_object).build_dto(domain_object)
+          strategy_for_domain_object(domain_object).build_dto(domain_object)
         end
       end
 
       def link_domain_object(domain_object, dto)
-        mapper_for_domain_object(domain_object).link_domain_object(domain_object, dto)
+        strategy_for_domain_object(domain_object).link_domain_object(domain_object, dto)
       end
 
       def build_new(dto)
-        mapper_for_dto(dto).build_new(dto)
+        strategy_for_dto(dto).build_new(dto)
       end
 
       def build_domain_object(dto)
-        mapper_for_dto(dto).build_domain_object(dto)
+        strategy_for_dto(dto).build_domain_object(dto)
       end
 
       def build_domain_objects(dtos)
         dtos.map do |dto|
-          mapper_for_dto(dto).build_domain_object(dto)
+          strategy_for_dto(dto).build_domain_object(dto)
         end
       end
 
       protected
-        def mapper_for_dto(dto)
-          mappers.find { |mapper| mapper.type_code == dto['type'] }
+        def strategy_for_dto(dto)
+          strategies.find { |strategy| strategy.type_code == dto['type'] }
         end
 
-        def mapper_for_domain_object(role)
-          mappers.find { |mapper| role.kind_of?(mapper.domain_object_class) }
+        def strategy_for_domain_object(role)
+          strategies.find { |strategy| role.kind_of?(strategy.domain_object_class) }
         end
 
       private
-        attr_reader :mappers
+        attr_reader :strategies
     end
   end
 end
